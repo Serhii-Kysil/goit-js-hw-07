57af47a544ae3bd1b5260a1459602689e254aca7
Serhii-Kysil <serik.kisel000@gmail.com>
2023-11-07 14:38:56 +0200

    add 1 markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+// console.log(galleryEl);
+
+function createMarkupGallery(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+  <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+	`
+    )
+    .join("");
+}
+galleryEl.insertAdjacentHTML("beforeend", createMarkupGallery(galleryItems));
+
+galleryEl.addEventListener("click", onImageClick);
+
+function onImageClick(e) {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  e.preventDefault();
+
+const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`)
+instance.show()
+
+}
+galleryEl.addEventListener('keydown',(e)=>{
+	if(e.code ==="Escape"){
+		instance.close();
+	}
+} )
